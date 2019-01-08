0a863a49ce53334ff37c0a62128fbbd964ebd182
Henrik Panhans <henrikpanhans@icloud.com>
2019-01-08 11:48:10 +0100

    Renamed all folder to match new project name

---
+++ HPOpenWeather/OpenWeatherSwift.h
@@ -0,0 +1,19 @@
+//
+//  OpenWeatherSwift.h
+//  OpenWeatherSwift
+//
+//  Created by Henrik Panhans on 11.02.17.
+//  Copyright © 2017 Henrik Panhans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenWeatherSwift.
+FOUNDATION_EXPORT double OpenWeatherSwiftVersionNumber;
+
+//! Project version string for OpenWeatherSwift.
+FOUNDATION_EXPORT const unsigned char OpenWeatherSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWeatherSwift/PublicHeader.h>
+
+
