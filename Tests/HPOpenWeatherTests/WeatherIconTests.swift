87e61d5e92530714f371008ee312ba8b144a84e2
Henrik Panhans <henrik@panhans.dev>
2020-12-15 23:33:31 +0100

    maybe fixing tests?

--- Tests/HPOpenWeatherTests/WeatherIconTests.swift
+++ Tests/HPOpenWeatherTests/WeatherIconTests.swift
@@ -3,15 +3,6 @@ import XCTest
 
 final class WeatherIconTests: XCTestCase {
 
-	#if canImport(AppKit)
-	@available(OSX 11.0, *)
-	func testAllSystemImages() {
-		WeatherIcon.allCases.forEach {
-			XCTAssertNotNil($0.filledNSImage())
-		}
-	}
-	#endif
-
 	#if canImport(UIKit)
 	@available(iOS 13.0, tvOS 13.0, *)
 	func testAllSystemImages() {
