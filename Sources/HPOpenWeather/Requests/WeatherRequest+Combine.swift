c7e9b936c34f5e02ac86906e172a36fbf3486c12
Henrik Panhans <henrik@panhans.dev>
2021-03-18 15:35:04 +0100

    Updating dependencies

--- Sources/HPOpenWeather/Requests/WeatherRequest+Combine.swift
+++ Sources/HPOpenWeather/Requests/WeatherRequest+Combine.swift
@@ -6,27 +6,12 @@ import Foundation
 public extension WeatherRequest {
 
 	func publisher(apiKey: String, language: RequestLanguage = .english, units: RequestUnits = .metric) -> AnyPublisher<Output, Error> {
-		let settings = OpenWeather.Settings(apiKey: apiKey, language: language, units: units)
-		return publisher(settings: settings)
+		publisher(settings: OpenWeather.Settings(apiKey: apiKey, language: language, units: units))
 	}
 
 	func publisher(settings: OpenWeather.Settings) -> AnyPublisher<Output, Error> {
 		let request = APINetworkRequest<Output>(url: makeURL(settings: settings), urlSession: urlSession, finishingQueue: finishingQueue)
 		return request.dataTaskPublisher()
-	}
-
-	static func error(from response: URLResponse?) -> Error? {
-		guard let response = response as? HTTPURLResponse else {
-			return nil
-		}
-
-		switch response.statusCode {
-		case 200...299:
-			return nil
-		default:
-			let errorCode = URLError.Code(rawValue: response.statusCode)
-			return URLError(errorCode)
-		}
 	}
 
 }
--- Sources/HPOpenWeather/Requests/WeatherRequest.swift
+++ Sources/HPOpenWeather/Requests/WeatherRequest.swift
@@ -6,53 +6,53 @@ public struct WeatherRequest {
 
 	// MARK: - Associated Types
 
-    public typealias Output = WeatherResponse
+	public typealias Output = WeatherResponse
 
 	// MARK: - Properties
 
-    public let coordinate: CLLocationCoordinate2D
+	public let coordinate: CLLocationCoordinate2D
 	public let excludedFields: [ExcludableField]?
-    public let date: Date?
-    public let urlSession: URLSession
-    public let finishingQueue: DispatchQueue
+	public let date: Date?
+	public let urlSession: URLSession
+	public let finishingQueue: DispatchQueue
 
 	// MARK: - Init
 
-    public init(
+	public init(
 		coordinate: CLLocationCoordinate2D,
 		excludedFields: [ExcludableField]? = nil,
 		date: Date? = nil,
 		urlSession: URLSession = .shared,
 		finishingQueue: DispatchQueue = .main
 	) {
-        self.coordinate = coordinate
+		self.coordinate = coordinate
 		self.excludedFields = excludedFields?.hp_nilIfEmpty()
-        self.date = date
-        self.urlSession = urlSession
-        self.finishingQueue = finishingQueue
-    }
+		self.date = date
+		self.urlSession = urlSession
+		self.finishingQueue = finishingQueue
+	}
 
 	// MARK: - OpenWeatherRequest
 
-    func makeURL(settings: OpenWeather.Settings) -> URL? {
-        URLBuilder.weatherBase
+	func makeURL(settings: OpenWeather.Settings) -> URL? {
+		URLBuilder.weatherBase
 			.addingPathComponent(date != nil ? "timemachine" : nil)
-            .addingQueryItem(coordinate.latitude, digits: 5, name: "lat")
-            .addingQueryItem(coordinate.longitude, digits: 5, name: "lon")
-			.addingQueryItem(date.flatMap({ Int($0.timeIntervalSince1970) }), name: "dt")
-			.addingQueryItem(excludedFields?.compactMap({ $0.rawValue }), name: "exclude")
-            .addingQueryItem(settings.apiKey, name: "appid")
-            .addingQueryItem(settings.units.rawValue, name: "units")
-            .addingQueryItem(settings.language.rawValue, name: "lang")
-            .build()
-    }
+			.addingQueryItem(name: "lat", value: coordinate.latitude, digits: 5)
+			.addingQueryItem(name: "lon", value: coordinate.longitude, digits: 5)
+			.addingQueryItem(name: "dt", value: date.flatMap({ Int($0.timeIntervalSince1970) }))
+			.addingQueryItem(name: "exclude", value: excludedFields?.compactMap({ $0.rawValue }))
+			.addingQueryItem(name: "appid", value: settings.apiKey)
+			.addingQueryItem(name: "units", value: settings.units.rawValue)
+			.addingQueryItem(name: "lang", value: settings.language.rawValue)
+			.build()
+	}
 
-    func makeNetworkRequest(settings: OpenWeather.Settings) throws -> APINetworkRequest<Output> {
+	func makeNetworkRequest(settings: OpenWeather.Settings) throws -> APINetworkRequest<Output> {
 		if let date = date, abs(date.timeIntervalSinceNow) <= 6 * .hour {
 			throw NSError.timeMachineDate
 		}
-        return APINetworkRequest(url: makeURL(settings: settings), urlSession: urlSession, finishingQueue: finishingQueue)
-    }
+		return APINetworkRequest(url: makeURL(settings: settings), urlSession: urlSession, finishingQueue: finishingQueue)
+	}
 
 }
 
